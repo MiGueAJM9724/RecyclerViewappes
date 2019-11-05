dc5913133ed787313c02a72d32cfd4e6cbe4f62c
Miguel Ángel Jiménez Meléndez <miguejm@outlook.es>
2019-11-05 17:51:23 -0600

    recycler

---
+++ app/src/main/java/com/example/appestudiantes/estudiante.kt
@@ -0,0 +1,15 @@
+package com.example.appestudiantes
+
+class estudiante (noControl: String, nomEst: String, carrera: String, edadEst: Int) {
+    var noCtrl: String = ""
+    var nomEst: String=""
+    var carrera: String =""
+    var edad: Int = 0
+
+    init{
+        this.noCtrl = noControl
+        this.nomEst = nomEst
+        this.carrera = carrera
+        this.edad = edadEst
+    }
+}
